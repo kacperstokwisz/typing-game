7c0eee92259accb1db07aea1e26c7eaf38683a7d
Kacper Stokwisz <kacperstokwisz@gmail.com>
2020-07-10 23:56:10 +0200

    add the timer

--- script.js
+++ script.js
@@ -43,6 +43,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+//Focus on text on start
+text.focus();
+
+//Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 //Generate random world from array
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
@@ -62,6 +68,27 @@ function updateScore() {
     scoreEl.innerHTML = score;
 }
 
+//Update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+    
+    if(time === 0) {
+        clearInterval(timeInterval);
+        //end game
+        gameOver();
+    }
+}
+
+//Game over, show end screen
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+}
+
 // Event listeners
 
 text.addEventListener('input', e => {
@@ -70,5 +97,7 @@ text.addEventListener('input', e => {
         addWordToDOM();
         updateScore();
         e.target.value = '';
+        time += 5;
+        updateTime();
     }
 });
